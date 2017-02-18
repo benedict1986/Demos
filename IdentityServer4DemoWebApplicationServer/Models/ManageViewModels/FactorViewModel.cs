7ff1abde0e6dbd39d8202e2046f113c41e2a0704
ben <benedict1986@gmail.com>
2017-02-18 20:45:06 +1100

    IdentityServer4DemoWebApplicationServer  and Client Demo

---
+++ IdentityServer4DemoWebApplicationServer/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer4DemoWebApplicationServer.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
