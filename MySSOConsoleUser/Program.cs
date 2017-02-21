456ac40d9d24c37749f4f7fd7c328656a08907f0
ben <benedict1986@gmail.com>
2017-02-21 23:13:06 +1100

    Protecting an API using Passwords

--- MySSOConsoleUser/Program.cs
+++ MySSOConsoleUser/Program.cs
@@ -19,8 +19,35 @@ namespace MySSOConsoleUser
             var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
 
             // request token
-            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
-            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+            //var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            //var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            //if (tokenResponse.IsError)
+            //{
+            //    Console.WriteLine(tokenResponse.Error);
+            //    return;
+            //}
+
+            //Console.WriteLine(tokenResponse.Json);
+            //Console.WriteLine("\n\n");
+
+            //// call api
+            //var client = new HttpClient();
+            //client.SetBearerToken(tokenResponse.AccessToken);
+
+            //var response = await client.GetAsync("http://localhost:5001/identity");
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(response.StatusCode);
+            //}
+            //else
+            //{
+            //    var content = await response.Content.ReadAsStringAsync();
+            //    Console.WriteLine(JArray.Parse(content));
+            //}
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");
 
             if (tokenResponse.IsError)
             {
